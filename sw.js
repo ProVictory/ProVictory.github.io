379e05af9d19675c3cf92887d574cfd6ebd5381f
ProVictory <pro.victory@yandex.ru>
2021-07-28 16:15:00 +0700

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'cache-v1'
+const precacheResources = [
+  '/'
+]
+
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!')
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(precacheResources)))
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!')
+})
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', async event => {
+    event.respondWith(new Promise(async (resolve, reject) => {
+        if (navigator.onLine)
+            resolve(fetch(event.request))
+        else
+            resolve(caches.match(event.request))
+    }))
+})
