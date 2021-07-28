167b17fc775837701c38211cb188b9d1bf60da7a
ProVictory <pro.victory@yandex.ru>
2021-07-28 18:52:49 +0700

    distance test

--- index.js
+++ index.js
@@ -18,9 +18,12 @@ if ('serviceWorker' in navigator) {
     })
 }
 
-let geoWatchID
+
 let geoToggle = document.querySelector('#geoToggle')
+let distEl = document.querySelector('#dist')
 let info = document.querySelector('#info')
+
+let geoWatchID
 geoToggle.addEventListener('click', () => {
     if(!geoWatchID) {
         geoWatchID = navigator.geolocation.watchPosition(success, error, options)
@@ -33,9 +36,15 @@ geoToggle.addEventListener('click', () => {
     }
 })
 
+let oldCoords
+let distance = 0
 
 function success(position) {
-    info.innerHTML += position.coords.latitude + ", " + position.coords.longitude + "<br>"
+    let coords = position.coords
+    info.innerHTML += coords.latitude + ", " + coords.longitude + "<br>"
+    if (oldCoords) distance += calcCrow(coords.latitude, coords.longitude, oldCoords.latitude, oldCoords.longitude)
+    distEl.textContent = distance + ' km'
+    oldCoords = position.coords
 } 
 function error() {
     alert('Sorry, no position available.')
@@ -44,4 +53,29 @@ const options = {
     enableHighAccuracy: true,
     maximumAge: 5000,
     timeout: 10000
+}
+
+Math.round((num + Number.EPSILON) * 100) / 100
+
+
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2) 
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
 }
